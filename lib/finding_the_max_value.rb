91c0b2e62700f542f4df706b98a857ca3b76c13f
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-05 12:56:46 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,6 +2,10 @@ def find_max_value(array)
   max_value = 0
   count = 0
   while count <= array.length do 
-    if max_value <count 
-      count = max_value
+    if max_value <array[count]
+      max_value = array[count]
+end
+count+=1
+end
+max_value
 end
